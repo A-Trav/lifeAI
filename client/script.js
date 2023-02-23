6228fca9a4c3db9a456d156e01f94dbc9f4d6553
Ashley Travaini <ashleytravaini@hotmail.com>
2023-02-24 08:55:40 +1000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://lifeai.onrender.com', {
+  const response = await fetch('https://lifeai-my99.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
